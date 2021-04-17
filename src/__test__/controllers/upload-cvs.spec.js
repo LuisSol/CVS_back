b829bf225d9620849adbc5edd63563a96f4ed217
Luis Enrique Solis Navarro <luis.solis.ext@geodis.com>
2021-04-16 19:05:32 -0500

    test(ci) - testing ci

---
+++ src/__test__/controllers/upload-cvs.spec.js
@@ -0,0 +1,37 @@
+const makeUploadCvs = require('../../controllers/upoad-cvs')
+
+describe('Upload CVS Controller', () => {
+  it('should call UC with the right data', async () => {
+    const result = 'Ok'
+    const uploadCVSUC = jest.fn(() => result)
+
+    const req = { body: {} }
+    const sendBuilder = { send: jest.fn() }
+    const res = { status: jest.fn(() => sendBuilder) }
+
+    const uploadCvsCtrl = makeUploadCvs({ useCases: { uploadCVSUC } })
+
+    await uploadCvsCtrl(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(sendBuilder.send).toHaveBeenCalledWith('Ok')
+  })
+
+  it('should return an error status code', async () => {
+    const result = 'Ok'
+    const uploadCVSUC = jest.fn(() => {
+      throw new Error('something went wrong')
+    })
+
+    const req = { body: {} }
+    const sendBuilder = { send: jest.fn() }
+    const res = { status: jest.fn(() => sendBuilder) }
+
+    const uploadCvsCtrl = makeUploadCvs({ useCases: { uploadCVSUC } })
+
+    await uploadCvsCtrl(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(sendBuilder.send).toHaveBeenCalledWith('something went wrong')
+  })
+})
